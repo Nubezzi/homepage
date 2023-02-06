3dddd21595a6ebeaae0a640054ae0e09b6a7f520
Paavo Nurminen <37696710+Nubezzi@users.noreply.github.com>
2023-02-06 19:24:31 +0200

    Shorten delays on about.svelte

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -8,8 +8,8 @@
 
 <div id="section-2" class='scroll-content'>
     {#if isIn}
-        <h2 transition:fly="{{delay: 50, duration: 600, x: 500, y: 0, opacity: 0, easing: cubicOut}}">About</h2>
-        <p transition:fly="{{delay: 200, duration: 600, x: 500, y: 0, opacity: 0, easing: cubicOut}}" >
+        <h2 transition:fly="{{delay: 20, duration: 400, x: 500, y: 0, opacity: 0, easing: cubicOut}}">About</h2>
+        <p transition:fly="{{delay: 100, duration: 600, x: 500, y: 0, opacity: 0, easing: cubicOut}}" >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec pretium tellus. Nam ac nibh orci. 
             Sed tempus, nibh ac dapibus dignissim, lorem leo gravida dui, vitae commodo dolor quam vitae lacus. Praesent vestibulum massa eget sapien cursus vulputate. 
             Nam rhoncus tellus nibh. Praesent interdum mattis eros at dictum. Etiam at porta odio. Ut vitae laoreet tortor. Proin convallis gravida orci, a lobortis justo finibus nec. 
